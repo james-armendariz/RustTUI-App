34546e18fb3f0c4dae98dccdf983ab89d1efe8a1
James Armendariz <jamesarmenda1128@gmail.com>
2025-11-03 12:08:26 -0500

    2. Build core git explorer with interactive commit browser

--- pipe/src/git.rs
+++ pipe/src/git.rs
@@ -19,3 +19,44 @@ pub fn get_recent_commits(count: usize) -> String
 
     String::from_utf8_lossy(&output.stdout).to_string()
 }
+
+#[derive(Debug, Clone)]
+pub struct Commit
+{
+    pub hash: String,
+    pub author: String,
+    pub date: String,
+    pub message: String,
+}
+
+pub fn get_commits(count: usize) -> Vec<Commit>
+{
+    let output = Command::new("git")
+        .args(&[
+            "log",
+            &format!("-{}", count),
+            "--pretty-format:%H|%an|%ad|%s",
+            "--date=short"
+        ])
+        .output()
+        .expect("Failed to execute git command");
+
+    let output_str = String::from_utf8_lossy(&output.stdout);
+
+    output_str
+        .lines()
+        .map(|line|
+        {
+            let parts: Vec<&str> = line.split('|').collect();
+            Commit
+            {
+                hash: parts[0].to_string(),
+                author: parts[1].to_string(),
+                date: parts[2].to_string(),
+                message: parts[3].to_string(),
+            }
+        })
+        .collect()
+
+    
+}
--- pipe/src/main.rs
+++ pipe/src/main.rs
@@ -1,8 +1,9 @@
 mod git;
 
 // Importing Dependencies
-use cursive::views::{Dialog, TextView, LinearLayout, Panel};
+use cursive::views::{Dialog, TextView, SelectView, LinearLayout, Panel};
 use cursive::{Cursive, CursiveExt};
+use git::Commit;
 
 fn main()
 {
@@ -11,13 +12,24 @@ fn main()
 
     // Get current directory's git info
     let branch = git::get_current_branch();
-    let recent_commits = git::get_recent_commits(5);
+    let commits = git::get_commits(10);
+
+    let mut select = SelectView::new();
+    for commit in commits
+    {
+        select.add_item(
+            format!("{} - {}", &commit.hash[..7], commit.message),
+            commit
+        );
+    }
+
+    select.set_on_submit(show_commit_details);
 
     let mut layout = LinearLayout::vertical();
 
     layout.add_child(TextView::new(format!("Branch: {}", branch)));
-    layout.add_child(TextView::new("\nRecent Commits:"));
-    layout.add_child(TextView::new(recent_commits));
+    layout.add_child(TextView::new(""));
+    layout.add_child(Panel::new(select).title("Commits"));
 
     siv.add_layer(
         Dialog::around(Panel::new(layout))
@@ -26,4 +38,21 @@ fn main()
     );
 
     siv.run();
+}
+
+fn show_commit_details(siv: &mut Cursive, commit: &Commit)
+{
+    let details = format!(
+        "Hash: {}\nAtuhor: {}\nDate: {}\nMessage: \n{}",
+        commit.hash, commit.author, commit.date, commit.message
+    );
+
+    siv.add_layer(
+        Dialog::around(TextView::new(details))
+            .title("Commit Details")
+            .button("Close", |s|
+            {
+                s.pop_layer();
+            }),
+    );
 }
